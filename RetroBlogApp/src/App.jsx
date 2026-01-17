a5bec9d96d2690381fd85508981add4b895a60eb
Albert CP <albertcp@gmail.com>
2026-01-17 18:04:05 +0530

    set up environment variables of appwrite

---
+++ RetroBlogApp/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    console.log(import.meta.env.VITE_APPWRITE_URL),
+    (
+      <>
+        <h1>Retro Blog App</h1>
+      </>
+    )
+  );
+}
+
+export default App;
