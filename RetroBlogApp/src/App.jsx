5dfa4ae7e0e2a9d880bae66357f95f9607345022
Albert CP <albertcp@gmail.com>
2026-01-26 17:32:33 +0530

    configured redux-toolkit

--- RetroBlogApp/src/App.jsx
+++ RetroBlogApp/src/App.jsx
@@ -1,14 +1,38 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    console.log(import.meta.env.VITE_APPWRITE_URL),
-    (
-      <>
-        <h1>Retro Blog App</h1>
-      </>
-    )
-  );
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main>Todo</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+}
 export default App;
